c3d4e287e63227cec052b580795ee080162f542e
Lara Ferrer <79275240+lara-ferrer@users.noreply.github.com>
2022-05-04 19:13:35 +0200

    feat: add maven workflow

--- src/main/java/com/laraferrer/wheretoeat/service/RestaurantService.java
+++ src/main/java/com/laraferrer/wheretoeat/service/RestaurantService.java
@@ -1,9 +1,15 @@
 package com.laraferrer.wheretoeat.service;
 
 import com.laraferrer.wheretoeat.domain.Restaurant;
+import com.laraferrer.wheretoeat.exception.RestaurantNotFoundException;
+import com.laraferrer.wheretoeat.dto.RestaurantPatchDTO;
 
 import java.util.List;
 
 public interface RestaurantService {
     List<Restaurant> findAllRestaurants();
+    Restaurant addRestaurant(Restaurant restaurant);
+    Restaurant modifyRestaurant(long restaurantId, Restaurant restaurant) throws RestaurantNotFoundException;
+    void patchRestaurant(long restaurantId, RestaurantPatchDTO restaurantPatchDTO) throws RestaurantNotFoundException;
+    void deleteRestaurantById(long restaurantId) throws RestaurantNotFoundException;
 }
