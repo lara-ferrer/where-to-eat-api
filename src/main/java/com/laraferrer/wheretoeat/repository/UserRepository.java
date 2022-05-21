32594495790ced393135cb9ec226f2049f0e80b5
Lara Ferrer <79275240+lara-ferrer@users.noreply.github.com>
2022-05-21 15:05:38 +0200

    feat: add user controller and model

---
+++ src/main/java/com/laraferrer/wheretoeat/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.laraferrer.wheretoeat.repository;
+
+import com.laraferrer.wheretoeat.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findAll();
+}
