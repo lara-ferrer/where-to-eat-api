8ef6113083f42f9e7f89c730d4c140763f80199e
Lara Ferrer <79275240+lara-ferrer@users.noreply.github.com>
2022-05-23 19:53:40 +0200

    feat: add getByName method (#23)
    * fix: change params naming
    
    * feat: handle exceptions in RestaurantController
    
    * feat: add findByName method
    
    * fix: remove unused imports

--- src/main/java/com/laraferrer/wheretoeat/service/RestaurantService.java
+++ src/main/java/com/laraferrer/wheretoeat/service/RestaurantService.java
@@ -1,6 +1,7 @@
 package com.laraferrer.wheretoeat.service;
 
 import com.laraferrer.wheretoeat.domain.Restaurant;
+import com.laraferrer.wheretoeat.dto.RestaurantDTO;
 import com.laraferrer.wheretoeat.exception.RestaurantNotFoundException;
 import com.laraferrer.wheretoeat.dto.PatchDTO;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 public interface RestaurantService {
     List<Restaurant> findAllRestaurants();
+    RestaurantDTO findNameById(long id) throws RestaurantNotFoundException;
     Restaurant addRestaurant(Restaurant restaurant);
     Restaurant modifyRestaurant(long restaurantId, Restaurant restaurant) throws RestaurantNotFoundException;
     void patchRestaurant(long restaurantId, PatchDTO patchDTO) throws RestaurantNotFoundException;
