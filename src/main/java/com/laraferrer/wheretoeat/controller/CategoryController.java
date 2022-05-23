f424ad8805da639c95f0d9e8f1fcf7dcb156df78
Lara Ferrer <79275240+lara-ferrer@users.noreply.github.com>
2022-05-23 21:20:48 +0200

    feat: add README.md

--- src/main/java/com/laraferrer/wheretoeat/controller/CategoryController.java
+++ src/main/java/com/laraferrer/wheretoeat/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.laraferrer.wheretoeat.controller;
 
 import com.laraferrer.wheretoeat.domain.Category;
+import com.laraferrer.wheretoeat.dto.CategoryDTO;
+import com.laraferrer.wheretoeat.dto.ErrorResponse;
 import com.laraferrer.wheretoeat.dto.PatchDTO;
 import com.laraferrer.wheretoeat.exception.CategoryNotFoundException;
 import com.laraferrer.wheretoeat.service.CategoryService;
@@ -22,6 +24,13 @@ public class CategoryController {
         categories = categoryService.findAllCategories();
 
         return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping(value = "/category/{categoryId}")
+    public ResponseEntity<CategoryDTO> getNameById(@PathVariable long categoryId) throws CategoryNotFoundException {
+        CategoryDTO categoryDTO = categoryService.findNameById(categoryId);
+
+        return ResponseEntity.ok(categoryDTO);
     }
 
     @PostMapping(value = "/category")
@@ -46,5 +55,17 @@ public class CategoryController {
     public ResponseEntity<Void> deleteCategory(@PathVariable long categoryId) throws CategoryNotFoundException {
         categoryService.deleteCategoryById(categoryId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(CategoryNotFoundException categoryNotFoundException) {
+        ErrorResponse errorResponse = new ErrorResponse(101, categoryNotFoundException.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse(102, "Error interno del servidor.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
