964bbaadf6dd40a6e1b0cec9057e5cf17f6c0d55
Lara <larafpics@gmail.com>
2022-09-03 23:06:37 +0200

    [fix]: fix bugs

--- src/main/java/com/laraferrer/wheretoeat/domain/User.java
+++ src/main/java/com/laraferrer/wheretoeat/domain/User.java
@@ -15,7 +15,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userId;
+    private long id;
     @Column
     private String username;
     @Column
