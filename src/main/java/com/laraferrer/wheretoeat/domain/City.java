9c03dac76fcdcce6a8f330c2699edeb301c250e3
Lara <larafpics@gmail.com>
2022-09-04 01:02:21 +0200

    [refactor]: refactor properties and DTOs

--- src/main/java/com/laraferrer/wheretoeat/domain/City.java
+++ src/main/java/com/laraferrer/wheretoeat/domain/City.java
@@ -15,7 +15,7 @@ public class City {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long cityId;
+    private long id;
     @Column
     private String name;
     @Column
